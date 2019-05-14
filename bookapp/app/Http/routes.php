c5bca56fabb0588020b6e391317a88bd25e436fe
First-一樹 Family-宮崎 <zakisan@miyazakikazukinoMacBook-Air.local>
2019-05-14 16:35:20 +0900

    First commit

---
+++ bookapp/app/Http/routes.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\BOOK;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+/*
+Route::get('/', function () {
+    return view('book');
+});
+*/
+Route::group(['middlewareGroups' => ['web']], function () {
+    Route::get('/', ['middleware' => 'auth',function(){
+        $books = Book::all();
+        return view('books',[
+            'books' => $books
+        ]);
+    }]);
+
+    Route::post('/book',['middleware' => 'auth',function(Request $request){
+        $validator  = Validator::make($request->all(),[
+            'name' => 'required|max:255',
+        ]);
+
+        if($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $book = new Book; //ORM
+        $book->title = $request->name;
+        $book->save();
+
+        return redirect('/');
+    }]);
+
+    Route::delete('/book/{book}',['middleware' => 'auth', function(Book $book){
+        $book->delete();
+
+        return redirect('/');
+    }]);
+
+    Route::auth();
+
+});
---
+++ hello.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+    <title>My First Web App</title>
+</head>
+<body>
+
+<h2>My First Web App</h2>
+<?php
+    echo "Hello World!\n";
+?>
+<br>
+<?php
+    echo "It runs on Web server!!\n"
+?>
+</body>
+</html>
